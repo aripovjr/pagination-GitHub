a5e18b6551d3230f1f3c48a156aedf6cfdcb2ebb
Jasurbek Aripov <jasurbek_aripov@mail.ru>
2022-02-05 15:43:15 +0300

    Create displayFollowers.js

---
+++ displayFollowers.js
@@ -0,0 +1,14 @@
+const conatiner = document.querySelector('.container')
+
+const display = followers =>{
+const newFollowers = followers.map(person=>{
+const {avatar_url, login, html_url} = person
+return `<article class="card">
+    <img src="${avatar_url}" alt="${login}"></img>
+    <h4>${login}</h4>
+    <a href="${html_url}" class="btn">view profile </a>
+</article>`
+}).join('')
+conatiner.innerHTML = newFollowers
+}
+export default display
