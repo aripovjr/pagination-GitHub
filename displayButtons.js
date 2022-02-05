7bd168b87ad9093b604a20e61ef3b0381519afcd
Jasurbek Aripov <jasurbek_aripov@mail.ru>
2022-02-05 20:41:48 +0300

    Update displayButtons.js

--- displayButtons.js
+++ displayButtons.js
@@ -1,6 +1,7 @@
 const displayButtons = (container, pages, activeIdx) => {
     let btns = pages.map((_,pageIdx)=>{
-        return `<button class="page-btn ${activeIdx === pageIdx?'active-btn':'null'} data-index="${pageIdx}">${pageIdx+1}</button>`
+        return `
+        <button class="page-btn ${activeIdx === pageIdx?'active-btn':'null '}" data-index="${pageIdx}">${pageIdx+1}</button>`
     })
     btns.push(`<button class="next-btn">next</button>`)
     btns.unshift(`<button class="prev-btn">prev</button>`)
