9b3ab0095c1d532e83c1e2deab3ce0047e61112d
Jasurbek Aripov <jasurbek_aripov@mail.ru>
2022-02-05 20:41:45 +0300

    Update displayFollowers.js

--- displayFollowers.js
+++ displayFollowers.js
@@ -4,7 +4,7 @@ const display = followers =>{
 const newFollowers = followers.map(person=>{
 const {avatar_url, login, html_url} = person
 return `<article class="card">
-    <img src="${avatar_url}" alt="${login}"></img>
+    <img src="${avatar_url}" alt='person'></img>
     <h4>${login}</h4>
     <a href="${html_url}" class="btn">view profile </a>
 </article>`
